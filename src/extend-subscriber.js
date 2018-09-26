fad47251f8f316a708c7a5dcb5113174548e5539
Evans Owino <evansowino@hotmail.com>
2018-09-26 20:48:27 +0100

    comments

--- src/extend-subscriber.js
+++ src/extend-subscriber.js
@@ -5,6 +5,7 @@ const observable$ = from(Util.numbers);
 
 // wraps the subscriber and transforms it before passing it on
 class DoubleSubscriber extends Subscriber {
+    // extend subscriber to override _next
     _next(value) {
         this.destination.next(value * 2);
     }
